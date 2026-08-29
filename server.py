73502ca0de0dde1e556015ada09c317d280ae635
srofail <sam.rofail@gmail.com>
2026-08-29 21:19:59 +1000

    refactor: improved readability, parsed phone number

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket, time, sys, os
 import json
 
+CALL_TIME = 10 #seconds
+
 def send_command(sock, command):
     obj = json.dumps({"command": command}).encode()
 
@@ -11,31 +13,36 @@ def send_command(sock, command):
     print("sending:", packet)
     sock.sendall(packet)
 
-s = socket.socket()
-s.connect(('127.0.0.1', 4444))
+# waits for caller, returns the number of the caller
+def wait_for_call(s: socket.socket):
+    while True:
+        data = s.recv(4096).decode(errors='ignore')
 
-print("brih")
+        json_text = data.split(":", 1)[1][:-1]  # remove "<length>:" and trailing ","
+        obj = json.loads(json_text)
 
-send_command(s, "reginfo")
+        print("received", json_text)
 
-while True:
-    data = s.recv(4096).decode(errors='ignore')
+        if "type" in obj and obj["type"] == "CALL_INCOMING":
+            return "0" + obj["peeruri"].split("@")[0][2:]
 
-    json_text = data.split(":", 1)[1][:-1]  # remove "<length>:" and trailing ","
-    obj = json.loads(json_text)
+def accept_call(s: socket.socket):
+    send_command(s, "accept")
+    print("answering")
 
-    print("received", json_text)
+    time.sleep(CALL_TIME)
 
-    if "type" in obj and obj["type"] == "CALL_INCOMING":
-        break
+    send_command(s, "hangup")
+    s.close()
 
-send_command(s, "accept")
-print("answering")
+def main():
+    s = socket.socket()
+    s.connect(('127.0.0.1', 4444))
+    send_command(s, "reginfo")
 
-CALL_TIME = 10
-time.sleep(CALL_TIME)
+    num = wait_for_call(s)
+    # TODO: check number against database
+    accept_call(s)
 
-send_command(s, "hangup")
-s.close()
-
-print("finished")
+if __name__ == "__main__":
+    main()
